72b080f4744bdf649cc48c949592f4897e29c884
Carly Donais <carlydonais@Carlys-MacBook-Pro.local>
2022-08-20 17:12:51 -0400

    created thought controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,24 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts: (req, res) =>{
+        Thought.find()
+          .select('-__v')
+          .then(thoughtData => res.json(thoughtData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    },
+    getSingleThought: (req, res) => {
+        const thoughtId = req.params.id;
+
+        Thought.findOne({ _id: thoughtId })
+          .then(thoughtData => res.json(thoughtData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+
+    }
+}
