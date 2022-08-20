211e4af904162670550f36bebb282fd37053f2c7
Carly Donais <carlydonais@Carlys-MacBook-Pro.local>
2022-08-20 13:40:18 -0400

    get and post route working for users

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,40 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/user-controller');
 
-router.use('/api', apiRoutes);
+/**
+ * Return all users
+ * GET /api/users
+ */
+router.get('/users', getAllUsers);
 
-router.get('/users', (req, res) => {
-    return res.send('in user route');
-})
+/**
+ * Return single user from database.
+ * GET /api/users/_id
+ */
+router.get('/users/:id', getSingleUser);
+
+/**
+ * Create new User
+ * POST /api/users
+ */
+router.post('/users', createUser);
+
+/**
+ * Update a user
+ * PUT /api/users/_id
+ */
+router.put('/users/:id', updateUser);
+
+/**
+ * Remove a user from the database
+ * DELETE /api/users/_id
+ */
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
