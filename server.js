2a18c35b21d26381ec30c1bd3d4f3e37db0de60e
Carly Donais <carlydonais@Carlys-MacBook-Pro.local>
2022-08-20 13:03:13 -0400

    added mongoose connection to server

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', 
+{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 mongoose.set('debug', process.env.NODE_ENV !== 'production');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes); 
-
+mongoose.connection.once('open', () => {
 app.listen(PORT, () => {
     console.log(`listening on localhost:${PORT}`)
-})
+    });
+});
